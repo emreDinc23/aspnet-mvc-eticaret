c0af98bea7f07c101a942c98cb71c55c91ffe839
Emre Dinç <105745981+emreDinc23@users.noreply.github.com>
2023-10-26 11:26:27 +0300

    Layout yapısına göre Ürünler, Ürün arama,Ürün detay sayfalarının ayarlanması

--- Uk.Eticaret.Web.Mvc/Controllers/ProductController.cs
+++ Uk.Eticaret.Web.Mvc/Controllers/ProductController.cs
@@ -8,10 +8,12 @@ namespace Uk.Eticaret.Web.Mvc.Controllers
         {
             return View();
         }
+
         public IActionResult Search(string query, int page)
         {
             return View();
         }
+
         public IActionResult Detail(int id)
         {
             return View();
