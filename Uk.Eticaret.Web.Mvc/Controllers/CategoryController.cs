17d84d1dc6d1f85939475de2ac0bea418aafc7f5
emreDinc23 <emre_2350@outlook.com>
2023-11-07 11:36:43 +0300

    Anasayfa verilerinin gösterimi

--- Uk.Eticaret.Web.Mvc/Controllers/CategoryController.cs
+++ Uk.Eticaret.Web.Mvc/Controllers/CategoryController.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Uk.Eticaret.EntityFramework;
+using Uk.Eticaret.EntityFramework.Entities;
 
 namespace Uk.Eticaret.Web.Mvc.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index(string slug, int page)
         {
             return View();
---
+++ Uk.Eticaret.Web.Mvc/Views/Shared/Components/Header/HeaderViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Uk.Eticaret.EntityFramework;
+using Uk.Eticaret.Web.Mvc.Models;
+
+namespace Uk.Eticaret.Web.Mvc.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public HeaderViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = _context.Categories
+                .Where(b => b.IsActive);
+
+            var categoriesDd = await categories
+                .Select(b => new SelectListItem
+                {
+                    Value = b.Id.ToString(),
+                    Text = b.Name
+                })
+                .OrderBy(b => b.Text)
+                .ToListAsync();
+
+            var vm = new HeaderViewModel()
+            {
+                CategoriesDd = categoriesDd,
+                Categories = categories.ToList(),
+            };
+
+            return View(vm);
+        }
+    }
+}
