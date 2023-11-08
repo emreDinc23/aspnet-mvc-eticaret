98f9b55d2956c2fe7696bac8501a400cddc7168e
emreDinc23 <emre_2350@outlook.com>
2023-11-08 11:19:30 +0300

    Slider İşlemleri

---
+++ Uk.Eticaret.Web.Mvc/Views/Shared/Components/Hero/HeroViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Uk.Eticaret.EntityFramework;
+
+namespace Uk.Eticaret.Web.Mvc.Views.Shared.Components.Heroo
+{
+    public class HeroViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public HeroViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var productsIsVisibleSlider = _context.Products.Where(pr => pr.IsVisibleSlider).ToList();
+            return View(productsIsVisibleSlider);
+        }
+    }
+}
