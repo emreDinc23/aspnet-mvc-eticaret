dd9147716dd33be2538bbaa96c6adfaf02712a04
Emre Dinç <105745981+emreDinc23@users.noreply.github.com>
2023-12-12 11:33:51 +0300

    Kullanıcı kaydı sayfası işlemleri (#24)
    Kullanıcı şifremi unuttum sayfası işlemleri

---
+++ Uk.Eticaret.Web.Mvc/Models/RegisterModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Uk.Eticaret.Web.Mvc.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "First Name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is required.")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password must match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ Uk.Eticaret.Web.Mvc/Services/PasswordReset/PasswordResetService.cs
@@ -0,0 +1,36 @@
+﻿using NETCore.MailKit.Core;
+
+namespace Uk.Eticaret.Web.Mvc.Services.PasswordReset
+{
+    public class PasswordResetService : IPasswordResetService
+    {
+        private readonly IEmailService _emailService;
+
+        public PasswordResetService(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        public async Task<string> GenerateResetTokenAsync(string userEmail)
+        {
+            // Şifre sıfırlama token'ını burada oluşturun (örneğin, bir GUID kullanabilirsiniz)
+            string resetToken = Guid.NewGuid().ToString();
+
+            // TODO: Reset token'ını veritabanında saklamak için ilgili işlemleri gerçekleştirin
+
+            return resetToken;
+        }
+
+        public async Task<bool> SendResetEmailAsync(string userEmail, string resetToken)
+        {
+            // E-posta gönderme işlemini burada gerçekleştirin
+            string subject = "Şifre Sıfırlama";
+            string body = $"Şifrenizi sıfırlamak için aşağıdaki bağlantıyı kullanın: /auth/resetpassword?email={userEmail}&resetToken={resetToken}";
+
+            // TODO: E-postayı göndermek için EmailService kullanın
+            await _emailService.SendAsync(userEmail, subject, body);
+
+            return true;
+        }
+    }
+}
